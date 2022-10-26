69700f8ade46c463fac6166ac7d89da3ea2261af
Abdel P <aperez4@godaddy.com>
2022-10-26 18:09:25 -0300

    delete slay folder

--- Slay/src/app.js
+++
@@ -1,9 +0,0 @@
-import slay from 'slay';
-
-const app = new slay.App(__dirname);
-app.start(options, function (err) {
-  if (err) { throw err; }
-  app.log.info(`Listening on ${app.config.get('http')}`);
-});
-
-const App = module.exports = app;
