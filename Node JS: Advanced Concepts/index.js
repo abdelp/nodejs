2a0a0a892415e1907be40c9660e594ace7914161
Abdel P <aperez4@godaddy.com>
2023-01-25 15:05:49 -0300

    Add pm2 package

--- Node JS: Advanced Concepts/index.js
+++ Node JS: Advanced Concepts/index.js
@@ -1,28 +1,17 @@
-process.env.UV_THREADPOOL_SIZE = 1
-const cluster = require('cluster')
+// I'm a child, I'm going to act like a server
+// and do nothing else
+const express = require('express')
+const crypto = require('crypto')
+const app = express()
 
-// Is the file being executed in master mode?
-if (cluster.isMaster) {
-  // Cause index.js to be executed *again* but
-  // in child mode
-  cluster.fork()
-  cluster.fork()
-} else {
-  // I'm a child, I'm going to act like a server
-  // and do nothing else
-  const express = require('express')
-  const crypto = require('crypto')
-  const app = express()
-
-  app.get('/', (req, res) => {
-    crypto.pbkdf2('a', 'b', 100_000, 512, 'sha512', () => {
-      res.send('Hi there')
-    })
+app.get('/', (req, res) => {
+  crypto.pbkdf2('a', 'b', 100_000, 512, 'sha512', () => {
+    res.send('Hi there')
   })
+})
 
-  app.get('/fast', (req, res) => {
-    res.send('This was fast!!')
-  })
+app.get('/fast', (req, res) => {
+  res.send('This was fast!!')
+})
 
-  app.listen(3000)
-}
+app.listen(3000)
